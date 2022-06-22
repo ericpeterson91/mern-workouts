5b42acfd64c3a5f7b98cf7a58448910afc826578
Eric Peterson <ericpeterson91@live.ca>
2022-06-22 12:22:10 -0600

    controller functions

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,21 +1,21 @@
 const express = require('express')
+const { 
+    createWorkout,
+    getWorkout,
+    getWorkouts
+} = 
+require('../controllers/workoutController')
 
 const router = express.Router()
 
 //get all workouts
-router.get('/', (req, res) => {
-    res.json({msg: 'Get all workouts'})
-})
+router.get('/', getWorkouts)
 
 //get single workout
-router.get('/:id', (req,res) => {
-    res.json({msg: 'Get a single workout'})
-})
+router.get('/:id', getWorkout)
 
-//post a new workout
-router.post('/', (req, res) =>{
-    res.json({msg: 'Post a new workout'})
-})
+// post a new workout
+router.post('/', createWorkout)
 
 //Delete a workout
 router.delete('/:id', (req, res) =>{
